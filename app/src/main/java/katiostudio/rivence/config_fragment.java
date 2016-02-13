d29946bf6b590cc56135be7284075201040bcc17
Akenas <ksender93@gmail.com>
2016-02-13 12:13:40 +0100

    UI CONFIG funcional

---
+++ app/src/main/java/katiostudio/rivence/config_fragment.java
@@ -0,0 +1,30 @@
+package katiostudio.rivence;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Kevin on 12/02/2016.
+ */
+public class config_fragment extends Fragment  {
+
+    public config_fragment() {
+        // Empty constructor required for fragment subclasses
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.config_fragment, container, false);
+
+        return rootView;
+    }
+
+}
+
+
+
+
---
+++ app/src/main/java/katiostudio/rivence/menu_fragment.java
@@ -0,0 +1,56 @@
+package katiostudio.rivence;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import static katiostudio.rivence.main.*;
+
+/**
+ * Created by Kevin on 12/02/2016.
+ */
+public class menu_fragment extends Fragment implements View.OnClickListener  {
+
+    public menu_fragment() {
+        // Empty constructor required for fragment subclasses
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.menu_fragment, container, false);
+        final ImageButton menu4 = (ImageButton) rootView.findViewById(R.id.Menu4);
+
+        menu4.setOnClickListener(this);
+        return rootView;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+
+        switch (v.getId()) {
+            case R.id.Menu4:
+                Fragment fragment = new config_fragment();
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(x, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.setCustomAnimations(android.R.animator.fade_in,
+                        android.R.animator.fade_out);
+                fragmentTransaction.commit();
+
+                break;
+
+        }
+    }
+}
+
+
+
+
